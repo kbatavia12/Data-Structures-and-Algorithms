3bcdc8c6907480aec0a5e2776026f13a2c5405bd
Karan Batavia <42770054+kbatavia12@users.noreply.github.com>
2021-05-12 17:05:24 +0530

    added recursive linear search

--- Algorithms/Searching/LinearSearch.cpp
+++ Algorithms/Searching/LinearSearch.cpp
@@ -13,8 +13,18 @@ int linearSearch(int val, vector<int> A) {
     return -1;
 }
 
-int main() {
-    vector<int> A = {1,2,3,4,5};
+int linearSearchRecursive(int val, int arr[]) {
+    //The main idea is to consider the array from each subsequent element
+    if (val == arr[0]) {
+        return 0; //Each element gets to become the zeroth element of the array
+    } else {
+        return linearSearchRecursive(val, arr+1) + 1;  //The total number of elements traversed get added up during the return time of the function
+    }
+}
 
-    cout << linearSearch(3, A);
+
+int main() {
+    int A[5] = {3,2,4,5,1};
+
+    cout << linearSearchRecursive(5, A);
 }
